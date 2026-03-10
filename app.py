d897a7f0c7a2d73dc9e1aebeecef39e8158bf8bd
mrgrassyt <mrgrass_1@inbox.lv>
2026-03-11 00:12:18 +0200

    Refactor app.py to improve structure and readability

--- app.py
+++ app.py
@@ -47,7 +47,6 @@ def buy(price):
     return redirect(url_for('index'))
 
 if __name__ == '__main__':
-if __name__ == '__main__':
     import os
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
